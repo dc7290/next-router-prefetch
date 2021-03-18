d8d52ffb0ff2e521b832e32fa5ef1f4d483cb40a
dc7290 <dhkh.cba0927@icloud.com>
2021-03-18 09:03:51 +0900

    libとescはgitで管理しない

--- esm/lib/prepareUrlAs.d.ts
+++
@@ -1,6 +0,0 @@
-import type { NextRouter } from "next/router";
-import type { Url } from "../useRouterPrefetch";
-export declare function prepareUrlAs(router: NextRouter, url: Url, as?: Url): {
-    url: string;
-    as: string;
-};
